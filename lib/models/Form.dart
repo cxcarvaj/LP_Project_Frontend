0a446afa30f29453d59cb13dd57e532583133a35
CarlosXavier <cxcarvaj@espol.edu.ec>
2020-09-06 16:03:17 -0500

    Api use in form

--- lib/models/Form.dart
+++ lib/models/Form.dart
@@ -2,10 +2,13 @@ class Formulario {
   static const String Male = "Male";
   static const String Female = "Female";
   static const String Lost = "Perdido";
+  static const String Need = "Necesitado";
+
 
 
   String petsName = '';
   String especie = '';
+  int edad = 0;
   Map<String, bool> sex = {
     Male: false,
     Female: false,
@@ -13,12 +16,14 @@ class Formulario {
   String desc = "";
   Map<String, bool> estados = {
     Lost: true,
+    Need: false,
   };
   double cordX = 0.0;
   double cordY = 0.0;
   String contacName = "";
   String cedula = "";
   String celular = "";
+
   save() {
     print('Guardando registro de mascota perdida!');
   }
--- lib/screens/form/form_screen.dart
+++ lib/screens/form/form_screen.dart
@@ -1,5 +1,123 @@
 import 'package:flutter/material.dart';
 import '../../models/Form.dart';
+import 'package:dio/dio.dart';
+
+void postPet(Formulario formulario) async {
+  var keyValueSex = formulario.sex.keys
+      .firstWhere((k) => formulario.sex[k] == true, orElse: () => null);
+
+  var keyValueEstado = formulario.estados.keys
+      .firstWhere((k) => formulario.estados[k] == true, orElse: () => null);
+  var dio = Dio();
+  try {
+    var response =
+        await dio.post("http://localhost:3000/api/pets/register", data: {
+      "name": formulario.petsName,
+      "especie": formulario.especie,
+      "gender": keyValueSex,
+      // "edad": formulario.edad,
+      "edad":12,
+      "caracteristica": formulario.desc,
+      "estado": keyValueEstado,
+      "state": true,
+      "contacto": formulario.contacName,
+      "telefono": formulario.celular,
+    });
+    return response.data;
+  } catch (e) {
+    print(e);
+  }
+}
+
+void postUbicacion(Formulario formulario) async {
+  var dio = Dio();
+  try {
+    var response =
+      await dio.post("http://localhost:3000/api/ubications/register", data: {
+      "id": "1234",//cambiar
+      "latitud": formulario.cordX,
+      "longitud": formulario.cordY,
+      "direccion": "Direccion",
+      "state": true,
+    });
+    return response.data;
+  } catch (e) {
+    print(e);
+  }
+}
+
+void postDataNeed(Formulario formulario) async {
+  var dio = Dio();
+  int petID = 0;
+  try {
+    Response response = await dio.get("http://localhost:3000/api/pets/consult");
+    var data1 = response.data;
+    for (var r in data1) {
+      if (r["name"] == formulario.petsName) {
+        petID = r["id"];
+      }
+    }
+  } catch (e) {
+    print(e);
+  }
+  try {
+    var response =
+        await dio.post("http://localhost:3000/api/needrecords/register", data: {
+      "username": "cxcarvaj",
+      "mascota": petID,
+      "tipo": "Carlos",
+      "state": true,
+      "contacto": formulario.contacName,
+      "correo":"admin12@hotmail.com",
+      "telefono": formulario.celular,
+    });
+    return response.data;
+  } catch (e) {
+    print(e);
+  }
+}
+
+void postData(Formulario formulario) async {
+  var keyValueSex = formulario.sex.keys
+      .firstWhere((k) => formulario.sex[k] == true, orElse: () => null);
+  var dio = Dio();
+  int petID = 0;
+  String dirID = "";
+  try {
+    Response response = await dio.get("http://localhost:3000/api/pets/consult");
+    Response response2 = await dio.get("http://localhost:3000/api/ubications/consult");
+    var data1 = response.data;
+    var data2 = response2.data;
+    for (var r in data1) {
+      if (r["name"] == formulario.petsName) {
+        petID = int.parse(r["id"]);
+      }
+    }
+    for (var d in data2) {
+      if (d["latitud"] == formulario.cordX &&
+          d["longitud"] == formulario.cordY) {
+        dirID = (d["id"]);
+      }
+    }
+  } catch (e) {
+    print(e);
+  }
+  try {
+    print(petID);
+    var response =
+      await dio.post("http://localhost:3000/api/lossrecords/register", data: {
+      "username": "cxcarvaj",
+      "mascota": petID,
+      "contacto": formulario.contacName,
+      "gender": keyValueSex,
+      "state": true,
+      "ubicacion": dirID,
+      "telefono": formulario.celular
+    });
+  } catch (e) {
+    print(e);
+  }
+}
 
 class HomeMaterial extends StatefulWidget {
   @override
@@ -99,6 +217,13 @@ class _HomeMaterialState extends State<HomeMaterial> {
                                       setState(() =>
                                           _form.estados[Formulario.Lost] = val);
                                     }),
+                                CheckboxListTile(
+                                    title: const Text('Necesitado'),
+                                    value: _form.estados[Formulario.Need],
+                                    onChanged: (val) {
+                                      setState(() =>
+                                          _form.estados[Formulario.Need] = val);
+                                    }),
                                 Text(""),
                                 Text(
                                   "Ubicación",
@@ -168,6 +293,20 @@ class _HomeMaterialState extends State<HomeMaterial> {
                                             form.save();
                                             _form.save();
                                             _showDialog(context);
+                                            var keyValueEstado =
+                                                _form.estados.keys.firstWhere(
+                                                    (k) =>
+                                                        _form.estados[k] ==
+                                                        true,
+                                                    orElse: () => null);
+                                            postPet(_form);
+                                            postUbicacion(_form);
+                                            if (keyValueEstado ==
+                                                "Necesitado") {
+                                              postDataNeed(_form);
+                                            } else {
+                                              postData(_form);
+                                            }
                                           }
                                         },
                                         child: Text('Enviar'))),
